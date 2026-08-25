986b257a103282525c0199a65c36a98a225a6b7f
Nilo Amor Urmeneta Jr. <97165586+niels012@users.noreply.github.com>
2026-08-25 23:34:24 +0800

    Update download URL in version.js

--- version.js
+++ version.js
@@ -1,4 +1,4 @@
 {
   "version": "1.1.0",
-  "download_url": "https://example.com/downloads/PP_Notes_Generator_Setup_v1.1.exe"
+  "download_url": "https://github.com/niels012/Hint-Notes-Generator-App/blob/main/Hint_Notes_Generator_Setup_v1.0.0.exe"
 }
